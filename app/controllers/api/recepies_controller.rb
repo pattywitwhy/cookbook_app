0e29c0e2e1ea6478ea4e90c97c11093db016b3d8
pattywitwhy <pattyxhwang@gmail.com>
2019-01-18 17:13:04 -0600

    updates

--- app/controllers/api/recepies_controller.rb
+++ app/controllers/api/recepies_controller.rb
@@ -1,6 +1,45 @@
 class Api::RecepiesController < ApplicationController
-  def one_recipe_action
-    @recipe = Recipe.last
-    render 'one_recipe_view.json.jbuilder'
+  def index
+    @recipes = Recipe.all
+    render 'index.json.jbuilder'
   end
+  
+  def create
+    @recipe = Recipe.new(
+                          title: params[:title],
+                          chef: params[:chef],
+                          ingredients: params[:ingredients],
+                          directions: params[:directions],
+                          prep_time: params[:prep_time]
+                        )
+    @recipe.save
+    render 'show.json.jbuilder'
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.title = params[:title] || @recipe.title
+    @recipe.chef = params[:chef] || @recipe.chef
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+    @recipe.image_url = params[:image_url] || @recipe.image_url
+
+    @recipe.save
+    render 'show.json.jbuilder'
+  end
+
+  def destory
+    recipe = Recipe.find(params[:id])
+    recipe.destory
+
+    render json: {message: "Successfully removed recipe."}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do 
-    get '/one_recipe_url' => 'recepies#one_recipe_action'
+    get '/recipes' => 'recepies#index'
+    post '/recipes' => 'recepies#create'
+    get '/recipes/:id' => 'recepies#show'
+    patch '/recipes/:id' => 'recepies#update'
+    delete '/recipes/:id' => 'recepies#destroy'
   end
 end
